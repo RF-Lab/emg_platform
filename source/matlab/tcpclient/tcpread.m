b17b6bf4189fa7c13e6be18f7ccc1b91ab21f625
estel1 <melnikov.aleksey@gmail.com>
2018-08-29 15:01:15 +0300

    Update tcpread.m

--- source/matlab/tcpclient/tcpread.m
+++ source/matlab/tcpclient/tcpread.m
@@ -1,10 +1,17 @@
 clc, clear ;
-t = tcpclient( '192.168.4.1', 8080) ;
+% ESP board address
+ip_addr = '192.168.4.1' ;
+tcp_port = 8080 ;
+t = tcpclient( ip_addr, tcp_port ) ;
 while t.BytesAvailable>=17
-    data = read(t,17) ;
-    if data(1)~=17
+    data = read(t,17) ; % read packet
+    if data(1)~=hex2dec('A5')
         break ;
     end
+    if data(2)~=hex2dec('5A')
+        break ;
+    end
+    % print packet counter
     fprintf( '%d\n', data(4)) ;
 end
-clear t ;
+clear t ; % close connection
