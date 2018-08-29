e2c15709d21b221a46ea3180519e5a87e8890804
estel1 <melnikov.aleksey@gmail.com>
2018-08-29 14:53:29 +0300

    Update tcpread.m

--- source/matlab/tcpclient/tcpread.m
+++ source/matlab/tcpclient/tcpread.m
@@ -2,6 +2,9 @@ clc, clear ;
 t = tcpclient( '192.168.4.1', 8080) ;
 while t.BytesAvailable>=17
     data = read(t,17) ;
+    if data(1)~=17
+        break ;
+    end
     fprintf( '%d\n', data(4)) ;
 end
 clear t ;
