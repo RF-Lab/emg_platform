b04456acd680255b1ad26752ea6f44532337aa03
dronluk <dronluk@yandex.ru>
2019-11-12 16:42:25 +0300

    Update create_spectrogramm.m

--- source/matlab/Create_specctrogramm_9_mov_6sub/create_spectrogramm.m
+++ source/matlab/Create_specctrogramm_9_mov_6sub/create_spectrogramm.m
@@ -1,18 +1,18 @@
-%Формирование спектрограмм
+%Г”Г®Г°Г¬ГЁГ°Г®ГўГ Г­ГЁГҐ Г±ГЇГҐГЄГІГ°Г®ГЈГ°Г Г¬Г¬
 %08.11.2019
-%Лукьянчиков А.И. dronluk@yandex.ru
+%Г‹ГіГЄГјГїГ­Г·ГЁГЄГ®Гў ГЂ.Г€. dronluk@yandex.ru
 clear all;
 close all;
-%папки с изображениями(если нет нужно создать)
+%ГЇГ ГЇГЄГЁ Г± ГЁГ§Г®ГЎГ°Г Г¦ГҐГ­ГЁГїГ¬ГЁ(ГҐГ±Г«ГЁ Г­ГҐГІ Г­ГіГ¦Г­Г® Г±Г®Г§Г¤Г ГІГј)
 dir_names_png={'png\train\1\'; 
-    '\png\train\2\';
-    '\png\train\3\';
-    '\png\train\4\';
-    '\png\train\5\';
-    '\png\train\6\';
-    '\png\train\7\';
-    '\png\train\8\';
-    '\png\train\9\';};
+    'png\train\2\';
+    'png\train\3\';
+    'png\train\4\';
+    'png\train\5\';
+    'png\train\6\';
+    'png\train\7\';
+    'png\train\8\';
+    'png\train\9\';};
 dir_names_png_test={'png\test\1\';
     'png\test\2\';
     'png\test\3\';
@@ -29,40 +29,40 @@ len_win=64;
 time = 40;
 len_fft=len_win*2;
 len_bin = ceil(len_fft/2);
-stap=10; % 84% перекрытия
-wind=hann(len_win); %окно Ханна
+stap=10; % 84% ГЇГҐГ°ГҐГЄГ°Г»ГІГЁГї
+wind=hann(len_win); %Г®ГЄГ­Г® Г•Г Г­Г­Г 
 size_table=size(nine_movs_six_sub_cut);
 size_cell=size(nine_movs_six_sub_cut{1,1}{1,1});
-%Формирование спектрограмм
-for i=1:size_table(1) %Номер субъекта без последнего, последний на тест
-    for j=1:size_cell(1) %Номер движения
+%Г”Г®Г°Г¬ГЁГ°Г®ГўГ Г­ГЁГҐ Г±ГЇГҐГЄГІГ°Г®ГЈГ°Г Г¬Г¬
+for i=1:size_table(1) %ГЌГ®Г¬ГҐГ° Г±ГіГЎГєГҐГЄГІГ  ГЎГҐГ§ ГЇГ®Г±Г«ГҐГ¤Г­ГҐГЈГ®, ГЇГ®Г±Г«ГҐГ¤Г­ГЁГ© Г­Г  ГІГҐГ±ГІ
+    for j=1:size_cell(1) %ГЌГ®Г¬ГҐГ° Г¤ГўГЁГ¦ГҐГ­ГЁГї
         size_ex=size(nine_movs_six_sub_cut{i,1}{1,1}{j,1});
-        for ex=1:size_ex(1) %номер испытания
-            x=nine_movs_six_sub_cut{i,1}{1,1}{j,1}(ex,:); % сигнал
+        for ex=1:size_ex(1) %Г­Г®Г¬ГҐГ° ГЁГ±ГЇГ»ГІГ Г­ГЁГї
+            x=nine_movs_six_sub_cut{i,1}{1,1}{j,1}(ex,:); % Г±ГЁГЈГ­Г Г«
             spectro=zeros(len_bin,time);
                 for k=0:time-1
                   if(len_win+(stap*k)>length(x))
                    break;
                   end
-                    frames(:,k+1)=x(1+(stap*k):len_win+(stap*k)); % выделение окна
-                    frames(:,k+1)=frames(:,k+1).*(wind); %оконное взвешивание
-                    tmp_spectro(:,k+1)=abs(fft(frames(:,k+1),len_fft)); % преобразование Фурье
+                    frames(:,k+1)=x(1+(stap*k):len_win+(stap*k)); % ГўГ»Г¤ГҐГ«ГҐГ­ГЁГҐ Г®ГЄГ­Г 
+                    frames(:,k+1)=frames(:,k+1).*(wind); %Г®ГЄГ®Г­Г­Г®ГҐ ГўГ§ГўГҐГёГЁГўГ Г­ГЁГҐ
+                    tmp_spectro(:,k+1)=abs(fft(frames(:,k+1),len_fft)); % ГЇГ°ГҐГ®ГЎГ°Г Г§Г®ГўГ Г­ГЁГҐ Г”ГіГ°ГјГҐ
                     spectro(:,k+1)=tmp_spectro(1:len_bin,k+1); % 
-                    spectro(:,k+1)=20*log10(spectro(:,k+1)); % логарифмическая шкала
+                    spectro(:,k+1)=20*log10(spectro(:,k+1)); % Г«Г®ГЈГ Г°ГЁГґГ¬ГЁГ·ГҐГ±ГЄГ Гї ГёГЄГ Г«Г 
 
                 end
             spectro=spectro(:,1:k);
-            spectro_max = max(max(abs(spectro))); % нормировка от 0 до 1
-            spectro_norm=uint16(((spectro./spectro_max)+1).*3.2768e+04); % перобразование в uint16
+            spectro_max = max(max(abs(spectro))); % Г­Г®Г°Г¬ГЁГ°Г®ГўГЄГ  Г®ГІ 0 Г¤Г® 1
+            spectro_norm=uint16(((spectro./spectro_max)+1).*3.2768e+04); % ГЇГҐГ°Г®ГЎГ°Г Г§Г®ГўГ Г­ГЁГҐ Гў uint16
             img=imshow(spectro_norm);
             if(i<=size_table(1)-1)
-                name_png=strcat(dir_names_png{j,1},num2str(i),'_',num2str(j),'_',num2str(ex),'.png'); % субъекты с 1 по 5
+                name_png=strcat(dir_names_png{j,1},num2str(i),'_',num2str(j),'_',num2str(ex),'.png'); % Г±ГіГЎГєГҐГЄГІГ» Г± 1 ГЇГ® 5
             else
-                name_png=strcat(dir_names_png_test{j,1},num2str(i),'_',num2str(j),'_',num2str(ex),'.png'); %6й субъект для теста
+                name_png=strcat(dir_names_png_test{j,1},num2str(i),'_',num2str(j),'_',num2str(ex),'.png'); %6Г© Г±ГіГЎГєГҐГЄГІ Г¤Г«Гї ГІГҐГ±ГІГ 
             end
             
             imwrite(spectro_norm, name_png, 'png')
-            fprintf('Sub= %d ; № mov= %d; № exper= %d  \n',i,j,ex);
+            fprintf('Sub= %d ; В№ mov= %d; В№ exper= %d  \n',i,j,ex);
         end
     end
 end
