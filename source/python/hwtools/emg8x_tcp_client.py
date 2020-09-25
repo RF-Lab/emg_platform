f057c5c4c42ead364bf73f6a9ad4f3b547ba4acf
Estel <melnikov.aleksey@rf-lab.org>
2020-09-25 23:50:45 +0300

    bytes fifo bug fixed

--- source/python/hwtools/emg8x_tcp_client.py
+++ source/python/hwtools/emg8x_tcp_client.py
@@ -33,6 +33,8 @@ import socket
 import sys
 from struct import *
 import matplotlib.pyplot as plt
+import numpy as np
+from scipy import signal
 
 EMG8x_ADDRESS                           = '192.168.1.65' ;
 
@@ -40,9 +42,10 @@ EMG8x_ADDRESS                           = '192.168.1.65' ;
 AD1299_NUM_CH                           =8
 TRANSPORT_BLOCK_HEADER_SIZE             =16
 PKT_COUNT_OFFSET                        =2
-SAMPLES_PER_TRANSPORT_BLOCK             =64
+SAMPLES_PER_TRANSPORT_BLOCK             =256
 TRANSPORT_QUE_SIZE                      =2
 TCP_SERVER_PORT                         =3000
+SAMPLES_TO_COLLECT                      = 4096
 
 TCP_PACKET_SIZE                         = int(((TRANSPORT_BLOCK_HEADER_SIZE)/4+(AD1299_NUM_CH+1)*(SAMPLES_PER_TRANSPORT_BLOCK))*4)
 
@@ -53,21 +56,68 @@ sock                = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Connect the socket to the port where the server is listening
 server_address      = (EMG8x_ADDRESS, TCP_SERVER_PORT)
 
+
 sock.connect(server_address)
+
+receivedBuffer                          = bytes()
+
+npSamples                               = np.zeros((SAMPLES_TO_COLLECT,))
+# Collected samples
+numSamples                              = 0 
 
 try:
     while True:
-        data = sock.recv(TCP_PACKET_SIZE)
-        if not data:
-            break 
-        samples = unpack('580i', data )
         
-        # get channel 4 samples
-        offset_to4ch    =  int(TRANSPORT_BLOCK_HEADER_SIZE/4 + SAMPLES_PER_TRANSPORT_BLOCK*4) 
+        if len(receivedBuffer)>=TCP_PACKET_SIZE*2:
+            
+            # find sync bytes
+            startOfBlock = receivedBuffer.find('EMG8x'.encode())
+            
+            if startOfBlock>=0:
         
-        #print( samples[offset_to4ch:offset_to4ch+SAMPLES_PER_TRANSPORT_BLOCK] )
-        plt.plot(samples[offset_to4ch:offset_to4ch+SAMPLES_PER_TRANSPORT_BLOCK])
-        plt.show
+                # SAMPLES_PER_TRANSPORT_BLOCK*(AD1299_NUM_CH+1)+TRANSPORT_BLOCK_HEADER_SIZE/4
+                samples                 = unpack('2308i', receivedBuffer[startOfBlock:startOfBlock+TCP_PACKET_SIZE] )
+                
+                print( 'Received block #{}'.format(samples[PKT_COUNT_OFFSET]) )
+                
+                # remove block from received buffer
+                receivedBuffer          = receivedBuffer[startOfBlock+TCP_PACKET_SIZE:] 
+                
+                # get channel 4 samples
+                offset_to4ch    =  int(TRANSPORT_BLOCK_HEADER_SIZE/4 + SAMPLES_PER_TRANSPORT_BLOCK*4) 
+                
+                #print( samples[offset_to4ch:offset_to4ch+SAMPLES_PER_TRANSPORT_BLOCK] )
+                dataSamples = samples[offset_to4ch:offset_to4ch+SAMPLES_PER_TRANSPORT_BLOCK]
+                
+                npSamples[numSamples:numSamples+SAMPLES_PER_TRANSPORT_BLOCK] = np.array(dataSamples)
+                numSamples += SAMPLES_PER_TRANSPORT_BLOCK
+                if numSamples>=SAMPLES_TO_COLLECT:
+                    break 
+        else:
+            receivedData                    = sock.recv( TCP_PACKET_SIZE )
+            if not receivedData:
+                # probably connection closed
+                break
+            
+            receivedBuffer += receivedData ;
         
+           
 finally:
     sock.close()
+    
+# remove corrupted samples
+# (not clear)
+npSamples[abs(npSamples)>100000] = 0
+
+plt.figure(1)    
+plt.plot(npSamples)
+plt.xlabel('Время, сек')
+plt.title('Канал №4')
+plt.grid('true')
+
+plt.figure(2)    
+frex,Pxx     = signal.welch( npSamples, fs=1024 )
+plt.semilogy( frex,Pxx )
+plt.xlabel('Частота, Гц')
+plt.title('Спектральная плотность мощности')
+plt.grid('true')
