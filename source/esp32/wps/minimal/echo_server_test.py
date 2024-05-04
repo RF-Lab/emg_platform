4cf9e2dd42372a8571a2c504dfb97f3ef7089e72
Mikhail <31549750+MikhailRibalkov@users.noreply.github.com>
2024-05-05 01:07:47 +0300

    Add files via upload

---
+++ source/esp32/wps/minimal/echo_server_test.py
@@ -0,0 +1,20 @@
+import socket
+from time import sleep
+
+def main():
+    interfaces = socket.getaddrinfo(host=socket.gethostname(), port=None, family=socket.AF_INET)
+    allips = [ip[-1][0] for ip in interfaces]
+
+    msg = b'EMG'
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)  # UDP
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    # sock.bind((ip,0))
+    sock.sendto(msg, ("255.255.255.255", 33333))
+    data, addr = sock.recvfrom(1024)
+    print(data, addr)
+    sock.close()
+
+
+
+main()
